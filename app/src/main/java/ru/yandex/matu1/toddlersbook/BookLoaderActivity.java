41e3f2a18b50982c4e3e79ad3f170595450e01a5
CoMatu <30466272+CoMatu@users.noreply.github.com>
2017-11-21 07:32:29 +0500

    new

--- app/src/main/java/ru/yandex/matu1/toddlersbook/BookLoaderActivity.java
+++ app/src/main/java/ru/yandex/matu1/toddlersbook/BookLoaderActivity.java
@@ -1,25 +1,37 @@
 package ru.yandex.matu1.toddlersbook;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.thin.downloadmanager.DownloadRequest;
+import com.thin.downloadmanager.ThinDownloadManager;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import ru.yandex.matu1.toddlersbook.models.Book;
+import ru.yandex.matu1.toddlersbook.models.BookFiles;
 
 public class BookLoaderActivity extends AppCompatActivity {
 
-    private ArrayList<Uri> BookPages;
-    private ArrayList<Uri> BookSounds;
     static final String TAG = "myLogs";
     private int bookId;
+    boolean CheckBookStorage = false;
+    String fileBookStorage;
 
     // "http://human-factors.ru/todbook/book1.json";
 
@@ -30,7 +42,71 @@ public class BookLoaderActivity extends AppCompatActivity {
 
         BookUriFromId();
 
-        new BookLoader().execute();
+        fileBookStorage = "book_"+bookId+".json";
+        CheckBookFile(fileBookStorage);
+        if(CheckBookStorage){
+            NextActivity();
+        }
+        BookLoader bookLoader = new BookLoader();
+        bookLoader.execute();
+        ArrayList<Uri> pagesPath;
+        ArrayList<Uri> soundsPath;
+        pagesPath = new ArrayList<>();
+        soundsPath = new ArrayList<>();
+        try {
+            String result = bookLoader.get();
+            Gson gson = new Gson();
+            Book book = gson.fromJson(result, Book.class);
+//            Log.d(TAG, result);
+            List<String> pages = book.getPageUrl();
+            String pagesUrl = String.valueOf(pages);
+
+            for(int i=0; i<pages.size(); i++){
+                Uri uri = Uri.parse(pages.get(i));
+                String fileN = uri.getLastPathSegment();
+                String filenam = getApplicationContext().getFilesDir()
+                        + File.separator + "book" + bookId +"_"+ fileN;
+            Log.d(TAG, filenam);
+                ThinDownloadManager downloadManager = new ThinDownloadManager(5); //количество потоков загрузки
+                Uri destinationUri = Uri.parse(filenam);
+                DownloadRequest downloadRequest = new DownloadRequest(uri).setDestinationURI(destinationUri);
+                downloadManager.add(downloadRequest);
+                pagesPath.add(destinationUri);
+            }
+
+//            Log.d(TAG, String.valueOf(pages));
+            List<String> sounds = book.getSoundUrl();
+            String soundsUrl = String.valueOf(sounds);
+
+            for(int i=0; i<sounds.size(); i++){
+                Uri uri = Uri.parse(sounds.get(i));
+                String fileNS = uri.getLastPathSegment();
+                String filenamS = getApplicationContext().getFilesDir()
+                        + File.separator + "book_" + bookId + fileNS;
+                Log.d(TAG, filenamS);
+                ThinDownloadManager downloadManager = new ThinDownloadManager(5); //количество потоков загрузки
+                Uri destinationUri = Uri.parse(filenamS);
+                DownloadRequest downloadRequest = new DownloadRequest(uri).setDestinationURI(destinationUri);
+                downloadManager.add(downloadRequest);
+                soundsPath.add(destinationUri);
+            }
+            BookFiles bookFiles = new BookFiles();
+            bookFiles.setBookID(bookId);
+            bookFiles.setPagesPath(pagesPath);
+            bookFiles.setSoundsPath(soundsPath);
+
+            Gson gson11 = new Gson();
+            String filesJson = gson11.toJson(bookFiles);
+
+            MyJSON.saveData(this, filesJson, fileBookStorage);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        NextActivity();
     }
 
     private class BookLoader extends AsyncTask<Void, Void, String>{
@@ -67,10 +143,6 @@ public class BookLoaderActivity extends AppCompatActivity {
             return resultJsonBook;
         }
 
-        @Override
-        protected void onPostExecute(String jsBook) {
-            super.onPostExecute(jsBook);
-        }
     }
 
     private void BookUriFromId(){
@@ -79,4 +151,28 @@ public class BookLoaderActivity extends AppCompatActivity {
         bookId = intent.getIntExtra("bookId", 1);
         Log.d(TAG, "You read book №" + bookId);
     }
+
+    private void CheckBookFile(String fileBookStorage) {
+        File fileOfBook = new File(getApplicationContext().getFilesDir().getPath() + "/" + fileBookStorage);
+
+        if (fileOfBook.exists()) {
+
+            CheckBookStorage = true;
+
+        }
+
+    }
+
+    private void NextActivity() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(BookLoaderActivity.this, SliderActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 20);
+    }
+
 }
---
+++ app/src/main/java/ru/yandex/matu1/toddlersbook/FileLoader.java
@@ -0,0 +1,17 @@
+package ru.yandex.matu1.toddlersbook;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import com.thin.downloadmanager.DownloadRequest;
+import com.thin.downloadmanager.ThinDownloadManager;
+
+import java.io.File;
+
+public class FileLoader {
+    public void GetBookId(String urlsBookFiles){
+
+    }
+}
